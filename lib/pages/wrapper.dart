ea5ae0be516152c0bdd827496aa9e46fd2c688e0
Jorge Zaragoza <jorge@grupodasana.com>
2021-09-10 08:53:28 -0700

    first commit

---
+++ lib/pages/wrapper.dart
@@ -0,0 +1,12 @@
+import 'package:flavors_test/pages/authenticate/authenticate.dart';
+// import 'package:flavors_test/pages/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Authenticate();
+  }
+}
