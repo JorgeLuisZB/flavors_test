f09d1d0c9d912e710ad70b06d7949642bdac48bf
jorge.zaragoza <jorge.zaragoza@ikontechnolgies.com>
2021-10-05 13:56:07 -0500

    Configuración de IOS

---
+++ lib/flavors/main_prod.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flavors_test/pages/wrapper.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      home: Wrapper(),
+    );
+  }
+}
